35825d7b8f5b2b2efad347a6a56898f1695f3f30
Edersondacosta <34005488+Edersondacosta@users.noreply.github.com>
2025-03-05 22:12:32 -0300

    Create adicaoMatriz.py
    Adição

---
+++ adicaoMatriz.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+# Definir matrizes
+A = np.array([[1, 2], [3, 4]])
+B = np.array([[5, 6], [7, 8]])
+c = 3
+
+# Operações com matrizes
+add_result = A + B
+
+# Imprimir resultados
+print("Adição de matrizes:\n", add_result)
