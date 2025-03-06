5e929ff2c41f22fa336db40ffee6bc9083bf0ff5
Edersondacosta <34005488+Edersondacosta@users.noreply.github.com>
2025-03-05 22:18:42 -0300

    Create multiplicaçãoEscalar.py
    multplicação

---
+++ multiplicaçãoEscalar.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Definir matrizes
+A = np.array([[1, 2], [3, 4]])
+B = np.array([[5, 6], [7, 8]])
+c = 3
+
+# Operações com matrizes
+scalar_mult_result = c * A
+
+
+# Imprimir resultados
+print("Multiplicação por escalar:\n", scalar_mult_result)
